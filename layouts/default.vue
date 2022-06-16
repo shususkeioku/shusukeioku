be10591dc23209cfefe7064a2e6cdb9fbfd696a8
si-zerodarkthirty <kyotan.equilibria@gmail.com>
2022-06-16 22:46:00 +0900

    init1

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Noto+San+Serif+JP&family=Ubuntu:wght@400;700&display=swap');
 body{
-  font-family: "Ubuntu", "Noto San Serif JP";
+  font-family: "Ubuntu", "Noto San Serif JP", sans-serif;
   background: rgb(23, 21, 21);
   color: rgb(181, 180, 180)
 }
