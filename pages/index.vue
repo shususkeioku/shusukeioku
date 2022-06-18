bc5ea3a38e0a4c67b2ffeeceb2b98175b6b60c86
si-zerodarkthirty <kyotan.equilibria@gmail.com>
2022-06-19 03:16:24 +0900

    init2

--- pages/index.vue
+++ pages/index.vue
@@ -59,7 +59,7 @@
 export default {
   async asyncData({ $content }) {
     const page = await $content('index').fetch()
-    const posts = await $content('blog').fetch()
+    const posts = await $content('blog').sortBy('date').fetch()
 
     return {
       page,
